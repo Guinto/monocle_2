c85b80e8ea73ac862042889d75a633da80d6753a
Trent Ellingsen <trent.ellingsen@mindbodyonline.com>
2013-05-01 01:11:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monocle2::Application.config.secret_token = '02e288676000140acc53a65a01606f41b49351b1cf75f9f8c4321652a63b771291b2785a52ed2a16f6d4f07a13e7f940e9bd75a985d22eb5b51c10664f352d26'
